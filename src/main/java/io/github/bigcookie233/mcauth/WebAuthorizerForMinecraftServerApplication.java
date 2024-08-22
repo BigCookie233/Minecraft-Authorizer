e1342ad7a44f6d88e8acc1aba1db2ae1a6c090cb
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2024-08-22 21:24:44 +0800

    Initial commit

---
+++ src/main/java/io/github/bigcookie233/mcauth/WebAuthorizerForMinecraftServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.bigcookie233.mcauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthorizerForMinecraftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAuthorizerForMinecraftServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bigcookie233/mcauth/WebAuthorizerForMinecraftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bigcookie233.mcauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAuthorizerForMinecraftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
